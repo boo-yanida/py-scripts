f11dbb3426d0c62a8196d75d841a8f6df0d84e20
Yanida Yanvudhi <boo.yanyanv@gmail.com>
2021-12-30 20:10:39 +0700

    Update html2markdown.py

--- code-challenges/html-to-md/html2markdown.py
+++ code-challenges/html-to-md/html2markdown.py
@@ -12,7 +12,7 @@ def html2markdown(html):
     markdown = re.sub(r'<em>(.*?)</em>',r'*\1*', html)  #italics
     markdown = re.sub(r'\s+',' ', markdown) #spaces
     markdown = re.sub(r'<p>(.*?)</p>',r'\1\n\n',markdown) #paragraphs
-    markdown = re.sub(r'<a href="(.*?)">(.*?)</a>',r'[\2](\1)',markdown)
+    markdown = re.sub(r'<a href="(.*?)">(.*?)</a>',r'[\2](\1)',markdown) #links
 
     markdown = markdown.strip()
     print(markdown)
